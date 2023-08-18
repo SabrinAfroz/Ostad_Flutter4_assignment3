05833e8aaf09ebe2eca6bbcc8fded83f4e592dcc
SabrinAfroz <sabrinafrozsathi@gmail.com>
2023-08-18 18:48:23 +0600

    Initial commit

---
+++ bin/car.dart
@@ -0,0 +1,52 @@
+class Car {
+  String? brand;
+  String? model;
+  int? year;
+  double milesDriven =0;
+  static int count = 0;
+
+  Car(String brand, String model , int year, double milesDriven){
+  this.brand = brand;
+  this. model = model;
+  this.year = year;
+  this.milesDriven = milesDriven;
+  count++;
+  }
+
+  void numberOfCars(){
+    print("Total number of cars created: ${Car.count}");
+  }
+
+
+  void drive(double miles) {
+    milesDriven += miles;
+
+  }
+
+  int getMilesDriven() {
+    return milesDriven.round();
+  }
+
+  String getBrand() {
+    return brand!;
+  }
+
+  String getModel() {
+    return model!;
+  }
+
+  int getYear() {
+    return year!;
+  }
+
+  int getAge() {
+
+    DateTime today = DateTime.now();
+    String year1 = "${today.year}";
+
+    int y = int.parse(year1);
+    int n = y - this.year!;
+
+    return n;
+  }
+}
---
+++ bin/carMain.dart
@@ -0,0 +1,16 @@
+import "car.dart";
+
+void main(){
+
+  Car c1 = Car("Toyota", "Camry",2020,10000);
+  Car c2 = Car("Honda", "Civic",2018,8000);
+  Car c3 = Car("Ford ", "F-150",2015,15000);
+
+
+  print("Car 1: ${c1.getBrand()} ${c1.getModel()} ${c1.getYear()} Miles: ${c1.getMilesDriven()} Age: ${c1.getAge()}");
+  print("Car 2: ${c2.getBrand()} ${c2.getModel()} ${c2.getYear()} Miles: ${c2.getMilesDriven()} Age: ${c2.getAge()}");
+  print("Car 3: ${c3.getBrand()} ${c3.getModel()} ${c3.getYear()} Miles: ${c3.getMilesDriven()} Age: ${c3.getAge()}");
+  c3.numberOfCars();
+
+
+}
